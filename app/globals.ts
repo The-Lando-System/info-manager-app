d6f5d98dd79e8662c1b0c7d5897e28d4db4bfbb1
Matt <matt.voget@gmail.com>
2017-04-02 15:42:12 -0600

    updated url

--- app/globals.ts
+++ app/globals.ts
@@ -1,4 +1,4 @@
 export class Globals {
-  svc_domain: string = 'https://info-manager.voget.io';
+  svc_domain: string = 'https://info-manager-svc.voget.io';
   //svc_domain: string = 'http://localhost:8090';
 }
