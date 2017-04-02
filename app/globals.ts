433fe897687bef847ffa05596d2dd4392399da3c
Matt <matt.voget@gmail.com>
2017-04-02 15:36:52 -0600

    updated globals

--- app/globals.ts
+++ app/globals.ts
@@ -1,4 +1,4 @@
 export class Globals {
-  svc_domain: string = 'https://info-manager.herokuapp.com';
+  svc_domain: string = 'https://info-manager.voget.io';
   //svc_domain: string = 'http://localhost:8090';
 }
