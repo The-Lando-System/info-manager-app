a6413488e0ab5030c059782156bc3c7abdfcbed9
Matt <matt.voget@gmail.com>
2017-04-01 21:40:11 -0600

    modified urls again

--- app/sarlacc-client/user.service.ts
+++ app/sarlacc-client/user.service.ts
@@ -14,8 +14,8 @@ export class UserService {
 
   private TAG = 'UserService - ';
 
-  private tokenUrl = 'http://sarlacc.mattvoget.com/oauth/token';
-  private userUrl = 'http://sarlacc.mattvoget.com/user-details';
+  private tokenUrl = 'http://sarlacc-svc.mattvoget.com/oauth/token';
+  private userUrl = 'http://sarlacc-svc.mattvoget.com/user-details';
 
   private token: Token;
   private user: User;
